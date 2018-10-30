d45138f9df86403821d93d7a1073e87e9b2d29be
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2018-10-30 14:59:48 -0400

    create execution with user_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,10 +41,11 @@ class ApplicationController < ActionController::API
 
   def api
     p params
-    par = params.permit(:text, :response_url, :user)
+    par = params.permit(:text, :response_url, :user, :user_id)
     text = par[:text]
     response_url = par[:response_url]
     user = par[:user]
+    user_id = par[:user_id]
     render json: {
       response_type: "in_channel",
       text: "Running code. Please wait.",
@@ -55,7 +56,7 @@ class ApplicationController < ActionController::API
       ]
     }
 
-    exec = Execution.create(user: user, command: text, response_url: response_url)
+    exec = Execution.create(user_id: user_id, user: user, command: text, response_url: response_url)
 
     Thread.new do
       Rails.application.executor.wrap do
